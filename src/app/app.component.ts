424ddc8d43330260eafe61a63be1b1e0646fb93b
Saitore973 <saitorevg@gmail.com>
2022-02-04 20:02:31 +0300

    create a class quote and display an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { Quote } from './quote';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes: Quote[] = [
+    { id: 1, name: 'Be the flame, not the moth' },
+    { id: 2, name: 'Above all, be the heroine of your life, not the victim.' },
+
+  ];
+
+  
 }
