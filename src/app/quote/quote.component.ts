aa40292b41209ead85e15375e0693c0bf8071651
Saitore973 <saitorevg@gmail.com>
2022-02-05 11:54:13 +0300

    add authors to the child component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,8 @@ import {Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    { id: 1, name: 'Be the flame, not the moth' },
-    { id: 2, name: 'Above all, be the heroine of your life, not the victim.' },
+    { id: 1, name: 'Be the flame, not the moth' , author: 'Casanova'},
+    { id: 2, name: 'Above all, be the heroine of your life, not the victim.', author: 'Nora Ephron'},
   ];
   constructor() {}
 
